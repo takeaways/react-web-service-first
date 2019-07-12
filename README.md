9953f06502a88e494573e02a16cecf62a34fe7f6
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2019-07-13 04:38:20 +0900

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-class => className 을 사용합니다.
-component!! 
---
+++ precache-manifest.407ff8db6df043c9a1c5226d4306cf78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5eff27ba5fc75e22af282305950ade",
+    "url": "/react-web-service-firs/index.html"
+  },
+  {
+    "revision": "fc25b56b02fc5bfd753e",
+    "url": "/react-web-service-firs/static/css/main.7214ae11.chunk.css"
+  },
+  {
+    "revision": "afaf07c70502d3a7ad8a",
+    "url": "/react-web-service-firs/static/js/2.14fd99e0.chunk.js"
+  },
+  {
+    "revision": "fc25b56b02fc5bfd753e",
+    "url": "/react-web-service-firs/static/js/main.9e8ff78f.chunk.js"
+  },
+  {
+    "revision": "33ccf1869b2c3bc12041",
+    "url": "/react-web-service-firs/static/js/runtime~main.9c867f0e.js"
+  }
+]);
