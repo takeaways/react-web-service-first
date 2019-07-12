a9ea15d1ce36d7ab612cd58dbc1047be07a4a6a4
GeonilJang <jgi0105@naver.com>
2019-07-13 02:40:03 +0900

    npm run build

--- src/App.js
+++ src/App.js
@@ -2,44 +2,50 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:"mov1",
-    poster:  "http://www.econovill.com/news/photo/201808/343844_214721_575.JPG",
-  },
-  {
-    title:"mov2",
-    poster:  "http://www.econovill.com/news/photo/201808/343844_214721_575.JPG",
-  },
-  {
-    title:"mov3",
-    poster:  "http://www.econovill.com/news/photo/201808/343844_214721_575.JPG",
-  },
-  {
-    title:"mov4",
-    poster:  "http://www.econovill.com/news/photo/201808/343844_214721_575.JPG",
-  },
-];
-
 function App() {
 //lifCycle
 // Render: componentWillMount() -> render() -> componentDidMount()
 // update: componentWillReceiveProps -> shouldComponentUpdate()
           // -> componentWillUpdate() -> render() -> componentDidUpdate()
-  const [greeting, setGreeting] = useState('Hello');
+  const [movies, setMovies] = useState(null);
 
-  //componentDidMount 
+  //componentDidMount
   useEffect(()=>{
-      setGreeting("Hello again");
-  },[])
+    _getMovies();
+  },[]);
+
+  const _getMovies = async () => {
+    const movies = await _callAPI();
+    setMovies(movies);
+  }
+
+  const _callAPI = () => {
+    const url = "https://yts.lt/api/v2/list_movies.json?sort_by=like_count"
+    return fetch(url)
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(e=>console.error(e));
+  }
+
+  const _renderMovies = () => {
+    return movies.map((movie, index) =>
+    <Movie
+      key={movie.id}
+      id={movie.id}
+      title={movie.title}
+      poster={movie.medium_cover_image}
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+    />)
+  }
 
   return (
     <div className="App">
-    {greeting}
-      { movies.length > 0 ? movies.map((movie, index)=><Movie key={index} title={movie.title} poster={movie.poster}/>) : "영화정보가 없습니다."}
+      {  movies ? _renderMovies() : "Loading..."}
     </div>
   );
 }
+
 
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
+import LineEllipsis from 'react-lines-ellipsis';
 
 
-function MoviePoster({poster}){
-  return(
-    <div>
-      <img src={poster}/>
-    </div>
-  )
+function MoviePoster({poster, title}){
+  return <img className="Movie__Poster"src={poster} alt={`${title} Movie Poster`}/>
 }
-
 MoviePoster.propTypes = {
   poster:PropTypes.string.isRequired,
+  title:PropTypes.string.isRequired,
 }
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
   return(
-    <div>
-      <h1>{title}</h1>
-      <MoviePoster poster={poster}/>
+    <div className="Movie">
+      <div className="Movie__Column">
+        <MoviePoster poster={poster} title={title}/>
+      </div>
+      <div className="Movie__Column">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          {genres.map((genre, i) => <span key={i} className="Movie__Genre"> {genre} </span>)}
+        </div>
+        <p className="Movie__Synopsis">
+          <LineEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+          />
+        </p>
+      </div>
     </div>
   )
 }
-
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
   poster:PropTypes.string.isRequired,
+  genres:PropTypes.array.isRequired,
+  synopsis:PropTypes.string.isRequired,
 }
 
 export default Movie;
